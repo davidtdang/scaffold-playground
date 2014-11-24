804eef20b05cbbe9e02de5cc240eb7f8822d2ade
David Dang <dtudang@gmail.com>
2014-11-24 14:54:30 -0800

    added location for organization and added show for both people and organizations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141124221618) do
+ActiveRecord::Schema.define(version: 20141124222457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20141124221618) do
     t.integer  "number_of_employees"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "locations"
   end
 
   create_table "people", force: true do |t|
